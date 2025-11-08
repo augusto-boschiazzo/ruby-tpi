d099124a3c664b6164c722d58c777115c8fee29e
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2025-11-07 21:50:00 -0300

    setup users with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_01_170017) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_07_231605) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -59,6 +59,22 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_01_170017) do
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_products_on_deleted_at"
     t.index ["product_category_id"], name: "index_products_on_product_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "role"
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
