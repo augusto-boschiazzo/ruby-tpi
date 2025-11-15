0fa7174b01d2d95c33e72fc6c9a8523493b1d4fb
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2025-11-15 19:24:39 -0300

    fix login and register forms css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  enum :role, [ :admin, :manager, :employee ]
+  enum :role, [ :admin, :manager, :employee, :customer ]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
