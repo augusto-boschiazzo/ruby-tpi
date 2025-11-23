993542bd759be0251d36ba5b41997fa7e69c1b6e
Matias Diaz <matyaslp_la12@hotmail.com>
2025-11-23 20:20:36 -0300

    se corrigen tests y fixtures

--- test/controllers/sales_controller_test.rb
+++ test/controllers/sales_controller_test.rb
@@ -3,46 +3,46 @@ require "test_helper"
 class SalesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @sale = sales(:one)
+    @user = users(:one)
   end
 
   test "should get index" do
+    sign_in @user
+
     get sales_url
     assert_response :success
   end
 
   test "should get new" do
+    sign_in @user
+
     get new_sale_url
     assert_response :success
   end
 
   test "should create sale" do
+    sign_in @user
+
+    client_attributes = { dni: 123456, name: "Pedro" }
+    item_sales_attributes = { "0": item_sales(:one).attributes.slice("product_id", "quantity", "unit_price") }
+
     assert_difference("Sale.count") do
-      post sales_url, params: { sale: { client_id: @sale.client_id, sold_at: @sale.sold_at, total_amount: @sale.total_amount, user_id: @sale.user_id } }
+      post sales_url, params: {
+        sale: {
+          user_id: @sale.user_id,
+          client_attributes: client_attributes,
+          item_sales_attributes: item_sales_attributes
+        }
+      }
     end
 
     assert_redirected_to sale_url(Sale.last)
   end
 
   test "should show sale" do
+    sign_in @user
+
     get sale_url(@sale)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_sale_url(@sale)
-    assert_response :success
-  end
-
-  test "should update sale" do
-    patch sale_url(@sale), params: { sale: { client_id: @sale.client_id, sold_at: @sale.sold_at, total_amount: @sale.total_amount, user_id: @sale.user_id } }
-    assert_redirected_to sale_url(@sale)
-  end
-
-  test "should destroy sale" do
-    assert_difference("Sale.count", -1) do
-      delete sale_url(@sale)
-    end
-
-    assert_redirected_to sales_url
   end
 end
