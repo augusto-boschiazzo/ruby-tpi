0fab57854f7360382314f7329be20d788472f913
tobiasbucheleogas <tobiasbucheleogas@gmail.com>
2025-12-04 22:25:19 +0000

    Test

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
   test "should get sales_summary" do
-    get reports_sales_summary_url
+    get sales_summary_reports_url
     assert_response :success
   end
 
   test "should get top_products" do
-    get reports_top_products_url
+    get top_products_reports_url
     assert_response :success
   end
 
   test "should get sales_by_employee" do
-    get reports_sales_by_employee_url
+    get sales_by_employee_reports_url
     assert_response :success
   end
 end
