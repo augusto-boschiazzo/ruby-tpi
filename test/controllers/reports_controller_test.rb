51cd4ed8910a9ad97288b7341580ed4bbef3860b
tobiasbucheleogas <tobiasbucheleogas@gmail.com>
2025-12-04 22:38:08 +0000

    Fix de reportes - Solo cuentan ventas activas

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get sales_summary" do
     get sales_summary_reports_url
     assert_response :success
