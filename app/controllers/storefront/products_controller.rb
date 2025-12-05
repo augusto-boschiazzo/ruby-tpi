8e0c8d42c5f5668739c432d85ed027e485dc9f2f
user <user@Ubuntu22.myguest.virtualbox.org>
2025-12-05 14:15:43 -0300

    Agrego varios productos al seed para probar fucionalidad de producots relacionados. Ahora se puede ingresar a un proucto en especifico

--- app/controllers/storefront/products_controller.rb
+++ app/controllers/storefront/products_controller.rb
@@ -1,6 +1,6 @@
 class Storefront::ProductsController < ApplicationController
     def index
-        @products = Product.includes(:images_attachments, :audio_attachment)
+        @products = Product.includes(:images_attachments)
                        .where(deleted_at: nil)
                        .order(created_at: :desc)
 
@@ -15,7 +15,51 @@ class Storefront::ProductsController < ApplicationController
             )
             end
         end
-        @products = @products.where(year: params[:year].to_i) if params[:year].present?
+        
+        if params[:year_from].present? || params[:year_to].present?
+
+            error_message = nil
+
+        unless params[:year_from].to_s.match?(/^\d*$/) && params[:year_to].to_s.match?(/^\d*$/)
+            error_message = "Ingrese un año válido (solo números)"
+        else
+            from = params[:year_from].to_i if params[:year_from].present?
+            to   = params[:year_to].to_i   if params[:year_to].present?
+
+            if from && to
+            if from <= to
+                @products = @products.where(year: from..to)
+            else
+                error_message = "El año 'Desde' no puede ser mayor que 'Hasta'"
+            end
+            elsif from
+            @products = @products.where("year >= ?", from)
+            elsif to
+            @products = @products.where("year <= ?", to)
+            end
+        end
+
+            flash.now[:alert] = error_message if error_message.present?
+
+        end
 
     end
+    def show
+        @product = Product
+               .includes(:images_attachments, :audio_attachment)
+               .find(params[:id])
+
+        @related_products = Product
+            .includes(:images_attachments)
+            .where.not(id: @product.id)
+            .where(deleted_at: nil)
+            .where(
+            "product_category_id = :cat OR product_type = :type OR author = :author",
+            cat: @product.product_category_id,
+            type: @product.product_type,
+            author: @product.author
+            )
+            .limit(6)
+    end
+
 end
