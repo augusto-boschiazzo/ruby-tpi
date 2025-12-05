5d83f568c34c60aae47ee1cca1f5a9e8ee9f3f69
user <user@Ubuntu22.myguest.virtualbox.org>
2025-12-05 15:23:30 -0300

    Vuevlo a modificar para que el home se vea la pagina welcome y los productos se ven en storefront/products

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "storefront/products#index"
+  root "pages#home"
 end
